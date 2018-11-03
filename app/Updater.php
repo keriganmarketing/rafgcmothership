6bee6d911e65aed9bb3aea1f329d739566b15f95
programmer-man <bryan@kerigan.com>
2018-11-02 22:09:52 -0400

    updater fix

--- app/Updater.php
+++ app/Updater.php
@@ -8,8 +8,7 @@ class Updater
         echo 'Updating Listings...';
         echo PHP_EOL;
         (new Listing)->getUpdates();
-        (new Navica('foo', 'foo', 'foo'))->connect()->buildPhotos();
-        MediaObject::labelPreferredImages();
+        (new Navica('foo', 'foo', 'foo'))->connect()->patchMissingPhotos();
         echo 'Done!' . PHP_EOL;
         (new Agent)->fullUpdate();
         (new Office)->fullUpdate();
