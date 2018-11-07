234beb9a93ba72e9cf3dab6c0f95e8f3e586333b
Daron Adkins <daron.adkins@gmail.com>
2018-11-07 16:01:45 -0500

    add queues and updater job

---
+++ app/Jobs/Update.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Updater;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class Update implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Updater::init();
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        Horizon::auth(function ($request) {
+            return true;
+        });
     }
 
     /**
