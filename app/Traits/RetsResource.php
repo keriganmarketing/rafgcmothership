a0883217dce6681dae0025df6815bb4607abaa2e
programmer-man <bryan@kerigan.com>
2018-11-19 20:33:42 -0500

    mapped lit_price column

--- app/Traits/RetsResource.php
+++ app/Traits/RetsResource.php
@@ -64,7 +64,7 @@ trait RetsResource {
     public function populateMasterTable()
     {
         $resource = new $this->local_resource;
-        $resource->chunk(200, function ($listings) use ($resource) {
+        $resource->chunk(1000, function ($listings) use ($resource) {
             foreach($listings as $listing) {
                 $columns = $resource::mapColumns($listing);
                 Listing::updateOrCreate(['mls_acct' => $columns['mls_acct']], $columns);
