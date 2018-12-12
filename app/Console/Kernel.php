96ebb49f69bfedaae25f9feaf39a021f791ba3d9
Bryan Baird <bryan@kerigan.com>
2018-12-11 21:26:55 -0500

    test scheduled jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(Update::dispatch())->onQueue('updaters')->hourly()->withOutOverlapping();
+        $schedule->job(new Update, 'updaters')->hourly()->withOutOverlapping();
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
