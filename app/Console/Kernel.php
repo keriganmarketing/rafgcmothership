7c6f2a3bbf7794c12dc34131ad492404951170e1
Bryan Baird <bryan@kerigan.com>
2019-02-12 12:53:28 -0500

    schedule Omni Updater

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Jobs\Update;
+use App\Jobs\UpdateOmni;
 use App\Jobs\CleanListings;
 use App\Jobs\SendNotification;
 use App\Jobs\LogImpression;
