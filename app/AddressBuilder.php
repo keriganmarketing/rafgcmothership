85613cdf2004b719c846e551ef6a20c5d9e77206
Bryan Baird <bryan@kerigan.com>
2019-04-30 15:46:26 -0400

    add rebuild address function

--- app/AddressBuilder.php
+++ app/AddressBuilder.php
@@ -24,4 +24,24 @@ class AddressBuilder
             }
         });
     }
+
+    public static function rebuildFullAddresses()
+    {
+        echo 'Populating full addresses' . PHP_EOL;
+        Listing::chunk(500, function ($listings) {
+        // DB::table('listings')->where('full_address', null)->orderBy('id', 'asc')->chunk(500, function ($listings) {
+            foreach ($listings as $listing) {
+                $streetNumber = $listing->street_num ?? '';
+                $streetName   = $listing->street_name ?? 'No Street Name Provided';
+                // $unit         = isset($listing->unit_num) && $listing->unit_num != '' ? ' ' . (int) $listing->unit_num : '';
+                $address      = (int) $streetNumber . ' ' . $streetName . ' ' . $listing->unit_num;
+
+                $listing->update([
+                    'full_address' => $address
+                ]);
+                //echo $listing->id . PHP_EOL;
+                echo '|';
+            }
+        });
+    }
 }
